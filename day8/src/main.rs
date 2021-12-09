b331be516fd9ec4bfd7a7c906866d98b6e6f2ace
Tony Gallotta <anthony.gallotta@gmail.com>
2021-12-08 19:38:30 -0600

    A little cleanup on Day 8

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,16 +1,32 @@
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 use std::io;
 use std::io::BufRead;
 use std::ops::Sub;
+use std::time::Instant;
+
+const ONE_LENGTH: usize = 2;
+const FOUR_LENGTH: usize = 4;
+const SEVEN_LENGTH: usize = 3;
+const EIGHT_LENGTH: usize = 7;
 
 fn main() {
+    let now = Instant::now();
     let filename = env::args().nth(1).unwrap_or(String::from("input.txt"));
     let parsed_lines = read_file_to_vec(filename);
     let part_1_answer = part1(&parsed_lines);
+    println!(
+        "Part 1 ({}ms): {}",
+        now.elapsed().as_millis(),
+        part_1_answer
+    );
     let part_2_answer = part2(&parsed_lines);
-    println!("Part 1: {}", part_1_answer);
-    println!("Part 2: {}", part_2_answer);
+    println!(
+        "Part 2 ({}ms): {}",
+        now.elapsed().as_millis(),
+        part_2_answer
+    );
 }
 
 fn read_file_to_vec(filename: String) -> Vec<String> {
@@ -27,7 +43,8 @@ fn read_file_to_vec(filename: String) -> Vec<String> {
 fn part1(lines: &Vec<String>) -> u64 {
     let mut unique_segment_count = 0;
     // # of segments in 1, 4, 7, 8
-    let unique_segment_sizes: Vec<usize> = vec![2, 4, 3, 7];
+    let unique_segment_sizes: HashSet<usize> =
+        HashSet::from([ONE_LENGTH, FOUR_LENGTH, SEVEN_LENGTH, EIGHT_LENGTH]);
     for line in lines {
         let output_part = line.split(" | ").nth(1).unwrap();
         unique_segment_count += output_part
@@ -39,130 +56,94 @@ fn part1(lines: &Vec<String>) -> u64 {
 }
 
 fn part2(lines: &Vec<String>) -> u64 {
-    lines.iter().map(|l| decode_segments(l)).sum()
+    lines.iter().map(|l| determine_output(l)).sum()
 }
 
-// Returns a vector of size 7 where the position of the element corresponds to a-g and the value
-// indicates what it is actually mapped to.
-fn decode_segments(line: &String) -> u64 {
-    let mut known_segments = vec!["x"; 10];
-    let mut input_output_parts = line.split("|");
-    let observations: Vec<&str> = input_output_parts
+// Returns the output value for the 7-segment display the line represents.
+fn determine_output(line: &String) -> u64 {
+    let mut identified_signal_patterns = vec!["x"; 10];
+    let mut observation_output_parts = line.split("|");
+    let observations: Vec<&str> = observation_output_parts
         .next()
         .unwrap()
         .split_ascii_whitespace()
         .collect();
-    let outputs: Vec<&str> = input_output_parts
+    let outputs: Vec<&str> = observation_output_parts
         .next()
         .unwrap()
         .split_ascii_whitespace()
         .collect();
-    const ONE_LENGTH: usize = 2;
-    const FOUR_LENGTH: usize = 4;
-    const SEVEN_LENGTH: usize = 3;
-    const EIGHT_LENGTH: usize = 7;
-    for observation in &observations {
-        // let chars = observation.chars().clone();
+    let mut undetermined_observations = HashSet::new();
+    // Determine the easy digits: 1, 4, 7 and 8
+    for (i, observation) in observations.iter().enumerate() {
         match observation.len() {
             ONE_LENGTH => {
-                // println!("Got a 1! {}", observation);
-                known_segments[1] = observation.clone();
+                identified_signal_patterns[1] = observation.clone();
             }
             FOUR_LENGTH => {
-                // println!("Got a 4! {}", observation);
-                known_segments[4] = observation.clone();
+                identified_signal_patterns[4] = observation.clone();
             }
             SEVEN_LENGTH => {
-                // println!("Got a 7! {}", observation);
-                known_segments[7] = observation.clone();
+                identified_signal_patterns[7] = observation.clone();
             }
             EIGHT_LENGTH => {
-                // println!("Got an 8! {}", observation);
-                known_segments[8] = observation.clone();
+                identified_signal_patterns[8] = observation.clone();
             }
-            _ => {}
+            _ => {
+                undetermined_observations.insert(i);
+            }
         }
     }
-    // A 9 contains all the segments of a 4, plus two more, so we can determine that now
-    // Notes on remaining digits
-    // 0 -> 6 AND contains both segments of 1
-    let chars_in_one: Vec<char> = known_segments[1].chars().collect();
-    let chars_in_four: Vec<char> = known_segments[4].chars().collect();
-    for observation in &observations {
+    // Determine the digits with 6 segments.
+    // 9 uniquely contains all the segments of a 4
+    // 0 uniquely contains all segments of 1
+    // 6 is the other one
+    for observation_index in undetermined_observations.clone().iter() {
+        let observation = observations[*observation_index];
         if observation.len() == 6 {
-            let chars_in_observation: Vec<char> = observation.chars().collect();
-            if chars_in_four
-                .iter()
-                .all(|c| chars_in_observation.contains(c))
-            {
-                known_segments[9] = observation.clone();
+            if contains_all_chars(observation, identified_signal_patterns[4]) {
+                identified_signal_patterns[9] = observation.clone();
+                undetermined_observations.remove(&observation_index.clone());
+            } else if contains_all_chars(observation, identified_signal_patterns[1]) {
+                identified_signal_patterns[0] = observation.clone();
+                undetermined_observations.remove(&observation_index.clone());
+            } else {
+                identified_signal_patterns[6] = observation.clone();
+                undetermined_observations.remove(&observation_index.clone());
             }
         }
     }
-    for observation in &observations {
-        if observation.len() == 6 && observation != &known_segments[9] {
-            let chars_in_observation: Vec<char> = observation.chars().collect();
-            if chars_in_one
-                .iter()
-                .all(|c| chars_in_observation.contains(c))
-            {
-                known_segments[0] = observation.clone();
-            }
+    // All remaining digits have 5 segments lit
+    // 3 contains all segments of 1
+    // 6 contains all segments of 5
+    for observation_index in undetermined_observations.clone().iter() {
+        let observation = observations[*observation_index];
+        if contains_all_chars(observation, identified_signal_patterns[7]) {
+            identified_signal_patterns[3] = observation.clone();
+            undetermined_observations.remove(&observation_index.clone());
+        } else if contains_all_chars(identified_signal_patterns[6], observation) {
+            identified_signal_patterns[5] = observation.clone();
+            undetermined_observations.remove(&observation_index.clone());
         }
     }
-    // 6 -> 6 is the other one of length 6
-    for observation in &observations {
-        if observation.len() == 6
-            && observation != &known_segments[0]
-            && observation != &known_segments[9]
-        {
-            known_segments[6] = observation.clone();
-        }
-    }
-    // 3 -> 5 AND contains all segments of 7
-    // 5 -> 5 Six contains all segments of 5 (2nd to last to determine)
-    let chars_in_seven: Vec<char> = known_segments[1].chars().collect();
-    let chars_in_six: Vec<char> = known_segments[6].chars().collect();
-    for observation in &observations {
-        if observation.len() == 5 {
-            let chars_in_observation: Vec<char> = observation.chars().collect();
-            if chars_in_seven
-                .iter()
-                .all(|c| chars_in_observation.contains(c))
-            {
-                known_segments[3] = observation.clone();
-            } else if chars_in_observation
-                .iter()
-                .all(|c| chars_in_six.contains(c))
-            {
-                known_segments[5] = observation.clone();
-            }
-        }
-    }
-    // 2 -> 5 the last one
-    for observation in &observations {
-        if observation.len() == 5
-            && observation != &known_segments[3]
-            && observation != &known_segments[5]
-        {
-            known_segments[2] = observation.clone();
-        }
-    }
-    for (i, c) in known_segments.iter().enumerate() {
-        println!("Determined {}=>{}", i, c);
-    }
+    // 2 is the last undetermined digit
+    identified_signal_patterns[2] =
+        observations[undetermined_observations.iter().next().unwrap().clone()];
+
+    // Now we know all the unique signal patterns and can decode the output
     let mut output_value: u64 = 0;
     for (i, output) in outputs.iter().enumerate() {
-        println!("Checking output {}", output);
-        let digit = known_segments
+        let digit = identified_signal_patterns
             .iter()
-            .position(|&v| {
-                v.chars().all(|c| output.contains(c)) && output.chars().all(|c| v.contains(c))
-            })
+            .position(|&v| contains_all_chars(v, output) && contains_all_chars(output, v))
             .unwrap();
         output_value += 10_u64.pow(3_u32.sub(i as u32)) * digit as u64;
     }
     output_value
+}
+
+fn contains_all_chars(container: &str, contained: &str) -> bool {
+    contained.chars().all(|c| container.contains(c))
 }
 
 #[test]
@@ -172,12 +153,21 @@ fn test_part1() {
 }
 
 #[test]
+fn test_contains_all_chars() {
+    assert!(contains_all_chars("abcd", "bac"));
+    assert!(!contains_all_chars("bac", "abcd"));
+}
+
+#[test]
 fn test_part2() {
     let sample_data = read_file_to_vec(String::from("test.txt"));
-    // assert_eq!(5353, decode_segments(&String::from(
-    //     "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf",
-    // )));
-    assert_eq!(5092, decode_segments(&String::from(
+    assert_eq!(
+        5353,
+        determine_output(&String::from(
+            "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf",
+        ))
+    );
+    assert_eq!(5092, determine_output(&String::from(
         "dg fadgceb dacbef agfeb gcdbef edcbf gdf ecgd cgbadf defbg | bedcf bgdfac cbfedg abfeg",
     )));
     assert_eq!(61229, part2(&sample_data));
