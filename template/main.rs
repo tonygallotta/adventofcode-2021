075fd55263003e83c66ca14be3d2d9ac966d82ab
Tony Gallotta <anthony.gallotta@gmail.com>
2021-12-12 10:14:45 -0600

    Adding a template

---
+++ template/main.rs
@@ -0,0 +1,54 @@
+use std::env;
+use std::fs;
+use std::io;
+use std::io::BufRead;
+use std::time::Instant;
+
+fn main() {
+    let now = Instant::now();
+    let filename = env::args().nth(1).unwrap_or(String::from("input.txt"));
+    let parsed_lines = read_file_to_vec(filename);
+    let part_1_answer = part1(&parsed_lines);
+    println!(
+        "Part 1 ({}ms): {}",
+        now.elapsed().as_millis(),
+        part_1_answer
+    );
+    let part_2_answer = part2(&parsed_lines);
+    println!(
+        "Part 2 ({}ms): {}",
+        now.elapsed().as_millis(),
+        part_2_answer
+    );
+}
+
+fn read_file_to_vec(filename: String) -> Vec<String> {
+    println!("Reading file {}", filename);
+    let file = fs::File::open(filename).unwrap();
+    let reader = io::BufReader::new(file);
+    let mut parsed_lines: Vec<String> = Vec::new();
+    for line in reader.lines() {
+        parsed_lines.push(line.unwrap());
+    }
+    parsed_lines
+}
+
+fn part1(lines: &Vec<String>) -> usize {
+    lines.len()
+}
+
+fn part2(lines: &Vec<String>) -> usize {
+    lines.len()
+}
+
+#[test]
+fn test_part1() {
+    let sample_data = read_file_to_vec(String::from("test.txt"));
+    assert_eq!(0, part1(&sample_data));
+}
+
+#[test]
+fn test_part2() {
+    let sample_data = read_file_to_vec(String::from("test.txt"));
+    assert_eq!(0, part2(&sample_data));
+}
